b1223d07792fecd01f96af58f7dc8dd85d4e4b78
zahra <zahratee@gmail.com>
2014-10-24 01:38:35 +0100

    fixed omission

--- registration/search.php
+++ registration/search.php
@@ -44,7 +44,7 @@
     }
     // Insert registration info
     // Retrieve data
-    $sql_select = "SELECT * FROM registration_tbl" " + $_POST['name']";
+    $sql_select = "SELECT * FROM registration_tbl";
     $stmt = $conn->query($sql_select);
     $registrants = $stmt->fetchAll(); 
     if(count($registrants) > 0) {
